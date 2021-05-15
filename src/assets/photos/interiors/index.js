58a54456181e52a9792516f6ccd1a52af5b2f904
cozyzoey <devzoeykim@gmail.com>
2021-05-15 12:03:26 +0900

    interior photos changed

--- src/assets/photos/interiors/index.js
+++ src/assets/photos/interiors/index.js
@@ -6,42 +6,35 @@ import interior3_jpg from './interior3.jpg'
 import interior3_webp from './interior3.webp'
 import interior4_jpg from './interior4.jpg'
 import interior4_webp from './interior4.webp'
-import interior5_jpg from './interior5.jpg'
-import interior5_webp from './interior5.webp'
-import interior6_jpg from './interior6.jpg'
-import interior6_webp from './interior6.webp'
 
 const interiors = [
-	{
-		webp: interior1_webp,
-		jpg: interior1_jpg,
-		alt: '입구',
-	},
-	{
-		webp: interior2_webp,
-		jpg: interior2_jpg,
-		alt: '들어오시는 길',
-	},
-	{
-		webp: interior3_webp,
-		jpg: interior3_jpg,
-		alt: '대기실',
-	},
-	{
-		webp: interior4_webp,
-		jpg: interior4_jpg,
-		alt: '인포데스크',
-	},
-	{
-		webp: interior5_webp,
-		jpg: interior5_jpg,
-		alt: '대기실',
-	},
-	{
-		webp: interior6_webp,
-		jpg: interior6_jpg,
-		alt: '상담실',
-	},
+  {
+    webp: interior1_webp,
+    jpg: interior1_jpg,
+    alt: '내부 모습',
+    title:
+      '입구로 들어오면 만나게 되는 온빛의 내부 모습입니다. <br/> 사진 왼편에 면담실이 자리하고 있습니다.',
+  },
+  {
+    webp: interior2_webp,
+    jpg: interior2_jpg,
+    alt: '휴게 공간',
+    title:
+      '자유롭게 이용하실 수 있는 휴게 공간을 마련했습니다. <br/> 편안하게 대기 및 휴식하실 수 있습니다.',
+  },
+  {
+    webp: interior3_webp,
+    jpg: interior3_jpg,
+    alt: '휴게 공간',
+    title:
+      '안쪽에도 마찬가지로 휴게 공간이 있습니다. <br /> 책장에 있는 도서는 대여하실 수 있습니다.',
+  },
+  {
+    webp: interior4_webp,
+    jpg: interior4_jpg,
+    alt: '면담실',
+    title: '면담을 하는 면담실입니다.',
+  },
 ]
 
 export default interiors
--- src/pages/Interior/Interior.jsx
+++ src/pages/Interior/Interior.jsx
@@ -1,56 +1,58 @@
 import './Interior.scss'
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
+import parse from 'html-react-parser'
 import { Helmet } from 'react-helmet'
 import { nanoid } from 'nanoid'
 import Fade from 'react-reveal/Fade'
 import { interiors } from '../../assets'
-
+import { logDOM } from '@testing-library/dom'
 
 function Interior() {
-	const elRef = useRef()
+  const elRef = useRef()
+  const [imgLoaded, setImgLoaded] = useState({})
 
-	useEffect(() => {
-		const onWheel = (e) => {
-			elRef.current.scrollLeft += e.deltaY
-		}
-		window.addEventListener('wheel', onWheel)
-		return () => window.removeEventListener('wheel', onWheel)
-	}, [])
+  useEffect(() => {
+    const onWheel = (e) => {
+      elRef.current.scrollLeft += e.deltaY
+    }
+    window.addEventListener('wheel', onWheel)
+    return () => window.removeEventListener('wheel', onWheel)
+  }, [])
 
-	const renderPhotos = () => {
-		return interiors.map((photo, index) => (
-			<div key={nanoid(12)} className='interior__item'>
-				<div className='interior__img'>
-					<picture>
-						<source type='image/webp' srcSet={photo.webp} />
-						<source type='image/jpeg' srcSet={photo.jpg} />
-						<img
-							src={photo.jpg}
-							alt={photo.alt}
-							width='100%'
-							height='100%'
-							loading='lazy'
-						/>
-					</picture>
-				</div>
-				<Fade right cascade>
-					<span className='interior__desc'>{photo.alt}</span>
-				</Fade>
-			</div>
-		))
-	}
+  const renderPhotos = () => {
+    return interiors.map((photo, index) => (
+      <div key={nanoid(12)} className="interior__item">
+        <div className="interior__img">
+          <picture>
+            <source type="image/webp" srcSet={photo.webp} />
+            <source type="image/jpeg" srcSet={photo.jpg} />
+            <img
+              src={photo.jpg}
+              alt={photo.alt}
+              width="100%"
+              height="100%"
+              loading="lazy"
+            />
+          </picture>
+        </div>
+        <Fade right cascade>
+          <span className="interior__desc">{parse(photo.title)}</span>
+        </Fade>
+      </div>
+    ))
+  }
 
-	return (
-		<div className='interior'>
-			<Helmet>
-				<title>공간소개 &ndash; 온빛정신건강의학과의원</title>
-				<meta name='robots' content='noindex' />
-			</Helmet>
-			<div className='interior--box' ref={elRef}>
-				{renderPhotos()}
-			</div>
-		</div>
-	)
+  return (
+    <div className="interior">
+      <Helmet>
+        <title>공간소개 &ndash; 온빛정신건강의학과의원</title>
+        <meta name="robots" content="noindex" />
+      </Helmet>
+      <div className="interior--box" ref={elRef}>
+        {renderPhotos()}
+      </div>
+    </div>
+  )
 }
 
 export default Interior
