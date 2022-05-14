fb3b6482f1d9f24e1dc40870170b9e1aa2795e6c
Zoey Kim <devzoeykim@gmail.com>
2022-05-14 22:21:22 +0900

    노드메일러 에러처리 추가

--- src/pages/Appointment/Email/Email.jsx
+++ src/pages/Appointment/Email/Email.jsx
@@ -6,128 +6,135 @@ import EdgeBtn from '../../../components/EdgeBtn'
 import Alert from '../../../components/Alert'
 
 function Email() {
-	const [name, setName] = useState('')
-	const [phone, setPhone] = useState('')
-	const [email, setEmail] = useState('')
-	const [content, setContent] = useState('')
-	const [emailSent, setEmailSent] = useState(false)
-	const [emailFail, setEmailFail] = useState(false)
-	const [emailLoading, setEmailLoading] = useState(false)
+    const [name, setName] = useState('')
+    const [phone, setPhone] = useState('')
+    const [email, setEmail] = useState('')
+    const [content, setContent] = useState('')
+    const [emailSent, setEmailSent] = useState(false)
+    const [emailFail, setEmailFail] = useState(false)
+    const [emailLoading, setEmailLoading] = useState(false)
 
-	useEffect(() => {
-		if (emailSent) {
-			setName('')
-			setPhone('')
-			setContent('')
-			setEmail('')
+    useEffect(() => {
+        if (emailSent) {
+            setName('')
+            setPhone('')
+            setContent('')
+            setEmail('')
 
-			const timer = setTimeout(() => setEmailSent(false), 3000)
-			return () => {
-				clearTimeout(timer)
-			}
-		} else if (emailFail) {
-			const timer = setTimeout(() => setEmailFail(false), 3000)
-			return () => {
-				clearTimeout(timer)
-			}
-		}
-	}, [emailSent, emailFail])
+            const timer = setTimeout(() => setEmailSent(false), 3000)
+            return () => {
+                clearTimeout(timer)
+            }
+        } else if (emailFail) {
+            const timer = setTimeout(() => setEmailFail(false), 3000)
+            return () => {
+                clearTimeout(timer)
+            }
+        }
+    }, [emailSent, emailFail])
 
-	const onInputChange = (e) => {
-		switch (e.target.name) {
-			case 'name':
-				setName(e.target.value)
-				break
-			case 'phone':
-				setPhone(e.target.value)
-				break
-			case 'email':
-				setEmail(e.target.value)
-				break
-			case 'content':
-				setContent(e.target.value)
-				break
-			default:
-				return
-		}
-	}
+    const onInputChange = (e) => {
+        switch (e.target.name) {
+            case 'name':
+                setName(e.target.value)
+                break
+            case 'phone':
+                setPhone(e.target.value)
+                break
+            case 'email':
+                setEmail(e.target.value)
+                break
+            case 'content':
+                setContent(e.target.value)
+                break
+            default:
+                return
+        }
+    }
 
-	const onEmailSubmit = (e) => {
-		e.preventDefault()
-		// === send email to firebase function
-		setEmailLoading(true)
-		const uri =
-			window.location.hostname === 'localhost'
-				? 'http://localhost:5001/onbitclinic/asia-northeast2/app/api/email'
-				: 'https://asia-northeast2-onbitclinic.cloudfunctions.net/app/api/email'
-		axios
-			.post(
-				uri,
-				{
-					email,
-					name,
-					content,
-					phone,
-				},
-				{ crossdomain: true }
-			)
-			.then((response) => setEmailSent(true))
-			.catch((error) => setEmailFail(true))
-			.finally(() => setEmailLoading(false))
-	}
+    const onEmailSubmit = (e) => {
+        e.preventDefault()
+        // === send email to firebase function
+        setEmailLoading(true)
+        const uri =
+            window.location.hostname === 'localhost'
+                ? 'http://localhost:5001/onbitclinic/asia-northeast2/app/api/email'
+                : 'https://asia-northeast2-onbitclinic.cloudfunctions.net/app/api/email'
+        axios
+            .post(
+                uri,
+                {
+                    email,
+                    name,
+                    content,
+                    phone,
+                },
+                { crossdomain: true }
+            )
+            .then((response) => {
+                if (response.data.includes('Error')) {
+                    throw Error('')
+                }
+                setEmailSent(true)
+            })
+            .catch((error) => setEmailFail(true))
+            .finally(() => setEmailLoading(false))
+    }
 
-	return (
-		<form className='email' onSubmit={onEmailSubmit}>
-			{emailSent && (
-				<Alert variant='success'>문의가 성공적으로 접수되었습니다.</Alert>
-			)}
-			{emailFail && (
-				<Alert variant='error'>
-					메일에 문제가 있습니다. 다른 방법으로 문의해 주세요.
-				</Alert>
-			)}
-			<input
-				type='text'
-				name='name'
-				value={name}
-				onChange={onInputChange}
-				placeholder='성함'
-				required
-				id='email-name-field'
-			/>
-			<input
-				type='tel'
-				name='phone'
-				value={phone}
-				onChange={onInputChange}
-				placeholder='연락처'
-				required
-			/>
-			<input
-				type='email'
-				name='email'
-				value={email}
-				onChange={onInputChange}
-				placeholder='이메일'
-				required
-			/>
-			<textarea
-				type='text'
-				name='content'
-				value={content}
-				onChange={onInputChange}
-				placeholder='상담 내용을 입력해주세요'
-				required
-				onKeyDown={resizeTextarea}
-				onKeyUp={resizeTextarea}
-			/>
-			<div className='email__btn'>
-				<EdgeBtn type='submit' disabled={emailLoading}>
-					{emailLoading ? 'Sending...' : '보내기'}
-				</EdgeBtn>
-			</div>
-		</form>
-	)
+    return (
+        <form className='email' onSubmit={onEmailSubmit}>
+            {emailSent && (
+                <Alert variant='success'>
+                    문의가 성공적으로 접수되었습니다.
+                </Alert>
+            )}
+            {emailFail && (
+                <Alert variant='error'>
+                    메일에 문제가 있습니다. 다른 방법으로 문의해 주세요.
+                </Alert>
+            )}
+            <input
+                type='text'
+                name='name'
+                value={name}
+                onChange={onInputChange}
+                placeholder='성함'
+                required
+                id='email-name-field'
+            />
+            <input
+                type='tel'
+                name='phone'
+                value={phone}
+                onChange={onInputChange}
+                placeholder='연락처'
+                required
+            />
+            <input
+                type='email'
+                name='email'
+                value={email}
+                onChange={onInputChange}
+                placeholder='이메일'
+                required
+            />
+            <textarea
+                type='text'
+                name='content'
+                value={content}
+                onChange={onInputChange}
+                placeholder='상담 내용을 입력해주세요'
+                required
+                onKeyDown={resizeTextarea}
+                onKeyUp={resizeTextarea}
+            />
+            <div className='email__btn'>
+                <EdgeBtn type='submit' disabled={emailLoading}>
+                    {emailLoading ? 'Sending...' : '보내기'}
+                </EdgeBtn>
+            </div>
+        </form>
+    )
 }
 
 export default Email
