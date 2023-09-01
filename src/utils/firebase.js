99f77fcc47e378b34eae35a16e79abf4d2d8c63d
cozyzoey <devzoeykim@gmail.com>
2023-09-01 19:25:25 +0900

    Hard code firebase projectId

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -7,7 +7,6 @@ import 'firebase/functions'
 const {
   REACT_APP_FIREBASE_API_KEY,
   REACT_APP_AUTH_DOMAIN,
-  REACT_APP_PROJECT_ID,
   REACT_APP_STORAGE_BUCKET,
   REACT_APP_MESSAGING_SENDER_ID,
   REACT_APP_APP_ID,
@@ -17,7 +16,7 @@ const {
 const firebaseConfig = {
   apiKey: REACT_APP_FIREBASE_API_KEY,
   authDomain: REACT_APP_AUTH_DOMAIN,
-  projectId: REACT_APP_PROJECT_ID,
+  projectId: 'onbitclinic',
   storageBucket: REACT_APP_STORAGE_BUCKET,
   messagingSenderId: REACT_APP_MESSAGING_SENDER_ID,
   appId: REACT_APP_APP_ID,
